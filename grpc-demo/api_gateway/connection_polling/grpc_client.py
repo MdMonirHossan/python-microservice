b585a923539eb3650184fe726a9cc2e8f35c9183
MdMonirHossan <mdraju01820@gmail.com>
2026-01-12 13:44:26 +0600

    Working on grpc client options for async setup

--- grpc-demo/api_gateway/connection_polling/grpc_client.py
+++ grpc-demo/api_gateway/connection_polling/grpc_client.py
@@ -1,5 +1,6 @@
 import grpc
 from generated_pb2 import payment_pb2_grpc
+from .options.grpc_client import GRPC_OPTIONS
 
 class PaymentClient:
     def __init__(self, target: str):
@@ -12,26 +13,7 @@ class PaymentClient:
             self._channel = grpc.aio.insecure_channel(
                 self._target,
                 # These control HTTP/2 keepalive pings between client ↔ server.
-                options=[
-                    # Send a keepalive ping every 30 seconds | Only if the connection is idle (no RPC traffic)
-                    # NATs, load balancers, firewalls silently drop idle TCP connections | This prevents “connection suddenly died” errors
-                    ("grpc.keepalive_time_ms", 30000),    # 30 Second
-
-                    # Wait 10 seconds for ping ACK | If not received → consider connection dead
-                    # Fast detection of dead peers | Avoid long hangs on broken connections
-                    ("grpc.keepalive_timeout_ms", 10000),   # 10 Second
-
-                    # Send keepalive pings even when no RPC is in progress
-                    # Prevent idle disconnects | Required for low-traffic services
-                    ("grpc.keepalive_permit_without_calls", 1), 
-                    
-                    # Forces periodic reconnection
-                    # Prevents stale / half-dead TCP connections
-                    # Important behind LBs & service meshes
-                    ("grpc.max_connection_idle_ms", 300000),      # 5 min
-                    ("grpc.max_connection_age_ms", 1800000),      # 30 min
-                    ("grpc.max_connection_age_grace_ms", 30000),  # 30 sec
-                ]
+                options=GRPC_OPTIONS
             )
             self._stub = payment_pb2_grpc.PaymentServiceStub(self._channel)
 
@@ -45,13 +27,4 @@ class PaymentClient:
             raise RuntimeError("PaymentClient not connected")
         return self._stub
     
-
-# grpc.aio.insecure_channel(
-#     target,
-#     options=[
-#         ("grpc.keepalive_time_ms", 30000),
-#         ("grpc.keepalive_timeout_ms", 10000),
-#         ("grpc.keepalive_permit_without_calls", 1),
-#     ],
-# )
 
--- grpc-demo/api_gateway/connection_polling/options/grpc_client.py
+++ grpc-demo/api_gateway/connection_polling/options/grpc_client.py
@@ -1,11 +1,11 @@
 GRPC_OPTIONS = [
     # Send a keepalive ping every 30 seconds | Only if the connection is idle (no RPC traffic)
     # NATs, load balancers, firewalls silently drop idle TCP connections | This prevents “connection suddenly died” errors
-    ("grpc.keepalive_time_ms", 30000),    # 30 Second
+    ("grpc.keepalive_time_ms", 30000),    # 30 sec
 
     # Wait 10 seconds for ping ACK | If not received → consider connection dead
     # Fast detection of dead peers | Avoid long hangs on broken connections
-    ("grpc.keepalive_timeout_ms", 10000),   # 10 Second
+    ("grpc.keepalive_timeout_ms", 10000),   # 10 sec
 
     # Send keepalive pings even when no RPC is in progress
     # Prevent idle disconnects | Required for low-traffic services
@@ -18,11 +18,14 @@ GRPC_OPTIONS = [
     ("grpc.max_connection_age_ms", 1800000),      # 30 min
     ("grpc.max_connection_age_grace_ms", 30000),  # 30 sec
 
-
+    # Prevent server from rejecting pings
+    # Avoid HTTP/2 GOAWAY errors
     ("grpc.http2.max_pings_without_data", 0),
-    ("grpc.http2.min_time_between_pings_ms", 10000),
-    ("grpc.http2.min_ping_interval_without_data_ms", 10000),
+    ("grpc.http2.min_time_between_pings_ms", 10000),         # 10 sec
+    ("grpc.http2.min_ping_interval_without_data_ms", 10000), # 10 sec
 
-    ("grpc.max_send_message_length", 10 * 1024 * 1024),
+    # Large payloads (statements, reconciliation)
+    # Prevent unexpected failures
+    ("grpc.max_send_message_length", 10 * 1024 * 1024),    # 10 MB
     ("grpc.max_receive_message_length", 10 * 1024 * 1024),
 ]

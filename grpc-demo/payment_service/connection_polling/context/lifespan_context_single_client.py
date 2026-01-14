b0f8ae8d8155b578f341b0e6c3d6e08f700f73bc
MdMonirHossan <mdraju01820@gmail.com>
2026-01-14 17:12:44 +0600

    Working on grpc client setup

--- grpc-demo/payment_service/connection_polling/context/lifespan_context_single_client.py
+++ grpc-demo/payment_service/connection_polling/context/lifespan_context_single_client.py
@@ -1,8 +1,10 @@
-import threading
+import grpc
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
+from generated_pb2 import payment_pb2_grpc
 
 from ..common.grpc.single_grpc_client import LedgerClient
+from ..grpc_server import PaymentService
 
 ledger_client = LedgerClient("localhost:50052")
 
@@ -19,16 +21,32 @@ async def lifespan(app:FastAPI):
     """
     # ============ Startup code ==============
     print("🚀 Application startup")
-
-    # start GRPC server without async
-    # threading.Thread(target=serve, daemon=True).start()
-    # asyncio.create_task(serve())
     
+    # Start gRPC client (Ledger)
     await ledger_client.connect()
+
+     # Start gRPC server (Payment)
+    grpc_server = grpc.aio.server()
+
+    payment_pb2_grpc.add_PaymentServiceServicer_to_server(
+        PaymentService(ledger_client),
+        grpc_server
+    )
+
+    grpc_server.add_insecure_port("[::]:50051")
+    await grpc_server.start()
+
+    app.state.grpc_server = grpc_server
 
     try:
         yield       # Application handles requests during this phase
     finally:
         # ============= Shutdown code ================
         print("🛑 Application shutdown")
+
+        # Stop gRPC server first (stop accepting requests)
+        await grpc_server.stop(grace=5)
+
+        # Close gRPC client channels
         await ledger_client.close()
+
--- grpc-demo/payment_service/connection_polling/main.py
+++ grpc-demo/payment_service/connection_polling/main.py
@@ -4,28 +4,9 @@ import grpc
 from .grpc_server import PaymentService
 from .common.grpc.single_grpc_client import LedgerClient
 from generated_pb2 import payment_pb2_grpc
+from .context.lifespan_context_single_client import lifespan 
 
-app = FastAPI()
-
-ledger_client = LedgerClient("localhost:50052")
-
-@app.on_event("startup")
-async def startup():
-    await ledger_client.connect()
-
-    server = grpc.aio.server()
-    payment_pb2_grpc.add_PaymentServiceServicer_to_server(
-        PaymentService(ledger_client), server
-    )
-    server.add_insecure_port("[::]:50051")
-    await server.start()
-
-    app.state.grpc_server = server
-
-@app.on_event("shutdown")
-async def shutdown():
-    await ledger_client.close()
-    await app.state.grpc_server.stop(5)
+app = FastAPI(title="Payment Service", lifespan=lifespan)
 
 @app.get("/health")
 async def health():

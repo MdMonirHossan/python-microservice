dc4258bfdf080a20cbb9404491bdcc2dfcae7b70
MdMonirHossan <mdraju01820@gmail.com>
2026-01-14 15:05:10 +0600

    Working on grpc client setup

---
+++ grpc-demo/payment_service/connection_polling/context/lifespan_context.py
@@ -0,0 +1,32 @@
+import threading
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
+from ..common.grpc.single_grpc_client import PaymentClient
+
+payment_client = PaymentClient("localhost:50051")
+
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    """
+    Async lifespan context manager for FastAPI to initialize and close resources..
+
+    - Startup: Connect to the grpc client
+    - Yield: hands control to the app while it serves requests.
+    - Shutdown: Close grpc client connection.
+    Parameters:
+    - app: FastAPI application instance.
+    """
+    # ============ Startup code ==============
+    print("🚀 Application startup")
+    
+    await payment_client.connect()
+
+    try:
+        yield       # Application handles requests during this phase
+    finally:
+        # ============= Shutdown code ================
+        print("🛑 Application shutdown")
+        await payment_client.close()
+
+    
